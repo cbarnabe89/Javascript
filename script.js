8849eb0658d898830216e439f20094cc2b3c83fb
cbarnabe89 <91102393+cbarnabe89@users.noreply.github.com>
2021-10-16 17:25:52 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function changeText(h1)
-{
-    document.getElementById('changeText').innerHTML = 'Logout';
-}
-
-document.getElementById("remove").onclick = function() {
-    document.getElementById("remove").remove();
-}

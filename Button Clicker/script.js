1315d628b25697e77eb244fb2a8dbd8d4120547a
cbarnabe89 <91102393+cbarnabe89@users.noreply.github.com>
2021-10-16 17:26:23 -0400

    Add files via upload

---
+++ Button Clicker/script.js
@@ -0,0 +1,8 @@
+function changeText(h1)
+{
+    document.getElementById('changeText').innerHTML = 'Logout';
+}
+
+document.getElementById("remove").onclick = function() {
+    document.getElementById("remove").remove();
+}

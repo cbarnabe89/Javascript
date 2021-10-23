16c51b6c0d14f7cf2a77e28118af73d5741e75fc
Barnabe <91102393+cbarnabe89@users.noreply.github.com>
2021-10-23 18:56:44 -0400

    Add files via upload

---
+++ Interactive profile-page/script.js
@@ -0,0 +1,21 @@
+var jd="Jane Doe"
+var changeName = document.querySelector("#jd");
+
+var ridof = document.querySelector('#remove')
+var ridof1 = document.querySelector('#remove1')
+
+function changeText()
+{
+    changeName.innerHTML = "Christine Barnabe"
+}
+
+function remove()
+{
+    ridof.remove()
+}
+
+function remove1()
+{
+    ridof1.remove()
+}
+
